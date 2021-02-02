9af050127345b9bdbab06d96e44b41a62ea4a59e
S.Kornev <post.kornev@gmail.com>
2021-02-02 22:40:20 +0300

    Initial Commit

---
+++ HW2.3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HW2.3
+//
+//  Created by Сергей Корнев on 02.02.2021.
+//  Copyright © 2021 Sergey Kornev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
